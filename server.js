d4cfda89c22c0b67516866c60449792bc3af794a
Jason Loomis <jason@jrloomis.com>
2019-10-31 14:40:25 -0400

    changed message for /

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ server.use(logger);
 server.use("/api/users", userRouter);
 server.use("/api/posts", postRouter);
 
-server.get("/", (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+// server.get("/", (req, res) => {
+//   res.send(`<h2>Let's write some middleware!</h2>`);
+// });
+
+server.get("/", (req, res) =>
+  res.status(200).json({ message: "It's working" })
+);
 
 //custom middleware
 
